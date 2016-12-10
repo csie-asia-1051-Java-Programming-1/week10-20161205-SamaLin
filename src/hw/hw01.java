963707dfc98e48800627cee71e6c2725503ddb09
SamaLin <105021058@live.asia.edu.tw>
2016-12-10 17:20:49 +0800

    week10-20161205-SamaLin

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,11 +1,65 @@
 package hw;
-import java.util.ArrayList;
+
+//import java.util.ArrayList;
+import java.util.Scanner;
 
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		String data[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			int oo = 0;
+			int xx = 0;
+			int sum = 0;
+			int v1 = scn.nextInt();
+			int v2 = scn.nextInt();
+			int v3 = scn.nextInt();
 
+			if (v1 > 60) {
+				oo++;
+				sum = sum + v1;
+			} else {
+				xx++;
+				sum = sum + v1;
+			}
+			if (v2 > 60) {
+				oo++;
+				sum = sum + v2;
+			} else {
+				xx++;
+				sum = sum + v2;
+			}
+			if (v3 > 60) {
+				oo++;
+				sum = sum + v3;
+			} else {
+				xx++;
+				sum = sum + v3;
+			}
+			
+			if(oo==3){
+				data[i]="P";
+			}else if(oo==2&&sum>=220){
+				data[i]="P";
+			}else if(oo==2&&sum<220){
+				data[i]="P";
+			}else if(oo==1){
+				if(v1>=80){data[i]="M";}
+				if(v2>=80){data[i]="M";}
+				if(v3>=80){data[i]="M";}
+			}else{
+				data[i]="F";
+			}
+		}
+		for(int i=0;i<n;i++){
+			System.out.println(data[i]);
+		}
+		
+		// public static int fun(int v1,int oo,int xx){
+		// return;
+		// }
+	}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,49 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int same = 0;
+		int data[] = new int[4];
+		int ram = 0;
+		for (int i = 0; i < 4; i++) {
+			data[i] = scn.nextInt();
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = i + 1; j < 4; j++) {
+				if (data[j] > data[i]) {
+					ram = data[i];
+					data[i] = data[j];
+					data[j] = ram;
+				}
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = i + 1; j < 4; j++) {
+				if (data[j] == data[i]) {
+					same = same + 1;
+				}
+			}
+		}
 
+		if (same == 1) {
+			for (int i = 0; i < 4 - 1; i++) {
+				if (data[i] == data[i + 1]) {
+					System.out.println(data[i] + data[i + 1]);
+				}
+			}
+		} else if (same == 2) {
+			System.out.println(data[0] + data[1]);
+		} else if (same == 3 || same == 0) {
+			System.out.println("R");
+		} else if (same == 6) {
+			System.out.println("³q±þ");
+		}
+//		System.out.println(same);
 	}
 
 }
